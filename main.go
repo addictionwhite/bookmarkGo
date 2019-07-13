8f941351f71959011622fbe6efd9e9023ff9d3bb
島敦士 <addictionwhite@gmail.com>
2019-07-13 19:31:51 +0900

    処理をメソッドに分割

--- main.go
+++ main.go
@@ -9,17 +9,47 @@ import (
 )
 
 func main() {
+    bookmarkGo()
+}
+
+
+func bookmarkGo(){
+    displayList()
+    fmt.Print("どのテキストを選択しますか")
+    var i int
+    fmt.Scan(&i)
+    choseOpenVim(i)
+}
+
+
+func choseOpenVim(i int){
+    ret := getBookmarkList()
+    slice := strings.Split(ret[i], ":")
+
+    // TODO 指定したindexがない場合の処理
+    cmd := exec.Command("vim", slice[1])
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    cmd.Run()
+}
+
+
+func getBookmarkList()[]string{
     // TODO 絶対パスを入れておかないとうまく動かない
     out, err := exec.Command("cat", "bookmark.txt").Output()
     if err != nil {
         fmt.Println(err.Error())
         os.Exit(1)
     }
-
     ret := strings.Split(string(out), "\n")
     //ret = ret[:len(ret)-1] // 不要な空行が入るので削除
+    return ret
+}
 
 
+func displayList(){
+    ret := getBookmarkList()
     for i, s := range ret {
         if len(s) > 70{
             tmp := strings.Split(ret[i], ":")
@@ -31,17 +61,5 @@ func main() {
             fmt.Printf("%d: %s\n", i, s)
         }
     }
-
-    fmt.Print("どのテキストを選択しますか")
-    var i int
-    fmt.Scan(&i)
-
-    slice := strings.Split(ret[i], ":")
-
-    // TODO 指定したindexがない場合の処理
-    cmd := exec.Command("vim", slice[1])
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.Run()
 }
+
